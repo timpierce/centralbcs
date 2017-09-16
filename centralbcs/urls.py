0076ff2fdd176629d90cd28979617b99fb23e3f3
Tim Pierce <timpierce.py@gmail.com>
2017-09-16 17:40:31 -0500

    supporting legacy f2f url

--- centralbcs/urls.py
+++ centralbcs/urls.py
@@ -6,6 +6,6 @@ admin.autodiscover()
 
 urlpatterns = [
     url(r'^agist/', include('agist.urls')),
-    url(r'^f2fsignup/', include('f2fsignup.urls')),
+    url(r'^/', include('f2fsignup.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
