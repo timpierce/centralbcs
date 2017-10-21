87abaded38ae196cd7e9b0bb248bae7ae7f977e5
Tim Pierce <timpierce.py@gmail.com>
2017-10-21 13:36:17 -0500

    supporting env variables on webfaction

--- centralbcs/wsgi.py
+++ centralbcs/wsgi.py
@@ -2,5 +2,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+os.environ.setdefault('RDS_DB_NAME', 'centralbcs')
+os.environ.setdefault('RDS_HOSTNAME', 'localhost')
+os.environ.setdefault('RDS_PORT', 3306)
+os.environ.setdefault('RDS_USERNAME', 'centralbcsdb')
+os.environ.setdefault('RDS_PASS', 'c0mpl3x!')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "centralbcs.settings")
 application = get_wsgi_application()
